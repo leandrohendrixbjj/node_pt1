e6a7bee8334839785ffd033e0ec9ba2a869e5fe3
hendrix <leandrotully@gmail.com>
2022-12-19 18:52:47 -0300

    Templates dinâmicos

--- src/Books/routes.js
+++ src/Books/routes.js
@@ -2,7 +2,19 @@ const route = require('express').Router();
 
 route.get('/', (req, res) => {
     res.marko(
-        require('../app/views/books/list.marko')
+        require('../app/views/books/list.marko'),
+        {
+            livros: [
+                {
+                    id: 1,
+                    titulo: 'Fundamentos do Node'
+                },
+                {
+                    id: 2,
+                    titulo: 'Node Avançado'
+                }
+            ]
+        }
     );
 });
 
